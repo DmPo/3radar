3d182e6effc05c805bd2befb7e6e7e1087fc8440
Dmytro Matsiuk <dmat@i.ua>
2016-11-20 20:03:59 +0200

    fixes

--- web/js/map.js
+++ web/js/map.js
@@ -13,19 +13,21 @@ function initMap() {
         $.get("/api/campaigns_markers", function (data) {
 
             for (var i = 0; i < data.length; i++) {
-                if (parseFloat(data[i].lat))
+                if (parseFloat(data[i].lat)) {
                     var marker = new google.maps.Marker({
                         position: {lat: parseFloat(data[i].lat), lng: parseFloat(data[i].lng)},
                         map: map,
                         title: data[i].major_name
                     });
-                google.maps.event.addListener(marker, 'click', (function (marker, i) {
-                    return function () {
-                        window.location = '#' + data[i].id;
-                        $('.panel').removeClass('active-panel');
-                        $('#' + data[i].id).addClass('active-panel');
-                    }
-                })(marker, i));
+                    google.maps.event.addListener(marker, 'click', (function (marker, i) {
+                        return function () {
+                            window.location = '#' + data[i].id;
+                            $('.panel').removeClass('active-panel');
+                            $('#' + data[i].id).addClass('active-panel');
+                        }
+                    })(marker, i));
+                }
+
             }
         });
     });
