c6241e143d072194ef8a232f9ce351d4ac3f8495
Dmytro Matsiuk <dmat@i.ua>
2016-11-20 19:07:03 +0200

    add validation

--- assets/views/new_campaign.php
+++ assets/views/new_campaign.php
@@ -9,7 +9,7 @@
                     <label>Вибіріть область:</label>
                     <select class="form-control" ng-model="campaign.region_id" name="region"
                             ng-change="load_districts()"
-                            ng-options="region.id as region.name for region in regions">
+                            ng-options="region.id as region.name for region in regions" required>
                         <option value="null" disabled selected> ---</option>
                     </select>
                 </div>
@@ -18,7 +18,7 @@
                     <label>Виберіть район / міську раду</label>
                     <select class="form-control" ng-model="campaign.district_id" name="district"
                             ng-change="load_councils()"
-                            ng-options="district.id as district.name for district in districts">
+                            ng-options="district.id as district.name for district in districts" required>
                         <option value="null" disabled selected> ---</option>
                     </select>
                 </div>
@@ -26,13 +26,13 @@
                 <div class="form-group">
                     <label>Рада</label>
                     <select class="form-control" ng-model="campaign.council_id" name="council" ng-change="show_info() "
-                            ng-options="council.id as council.name for council in councils">
+                            ng-options="council.id as council.name for council in councils" required>
                         <option value="null" disabled selected> ---</option>
                     </select>
                 </div>
                 <div class="panel" ng-if="council_info">
                     <div class="panel-body">
-                        <ul>
+                        <ul class="major-info">
                             <li ng-if="council_info.major_name"><strong>Мер / Сільський голова: </strong> <span
                                     ng-bind="council_info.major_name"></span></li>
                             <li ng-if="council_info.major_bio"><strong>Біографія: </strong> <span
@@ -44,7 +44,7 @@
                 </div>
                 <div class="form-group">
                     <label>Причина</label>
-                    <textarea ng-model="campaign.reason" name="reason" rows="5" class="form-control"></textarea>
+                    <textarea ng-model="campaign.reason" name="reason" rows="5" class="form-control" required></textarea>
                 </div>
 
                 <div class="form-group">
@@ -72,7 +72,7 @@
                     </label>
                 </div>
                 <div class="text-center">
-                    <button class="btn btn-default" type="button" ng-click="new_campaign()">Відкликати</button>
+                    <button class="btn btn-success" type="button" ng-click="new_campaign()" ng-disabled="campaign_form.$invalid">Відкликати</button>
                 </div>
         </div>
     </div>
