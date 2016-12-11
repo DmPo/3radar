6c1490b3c5edda242f342ce0dad7a779e47e11c5
DmPo <dimopot@gmail.com>
2016-12-11 19:28:11 +0200

    Update newpass.php

--- assets/views/newpass.php
+++ assets/views/newpass.php
@@ -2,13 +2,13 @@
     <div class="section-overlay"></div>
     <div class="form-wrapper">
         <div class="form">
-            <h2 class="h2">Введіть новий пароль</h2>
+            <h2 class="h2">Enter new password</h2>
             <form class="login-form" method="post">
                 <input placeholder="Новий пароль" name="password"/>
                 <?php if(isset($error)):?>
                     <p style="color: red; font-size: medium"> <?=$error?> </p>
                 <?php endif;?>
-                <button>Скинути пароль</button>
+                <button>Reset password</button>
             </form>
         </div>
     </div>
