3dc53a465ffcf1011ce7b93333399085c8798860
DmPo <dimopot@gmail.com>
2016-12-11 18:14:42 +0200

    Update hello.php

--- assets/views/hello.php
+++ assets/views/hello.php
@@ -49,7 +49,7 @@
                 <li>
                     <img src='https://static.tildacdn.com/0efe83b2-ea85-41be-a16d-e491339a8559/placel.png'>
                     <h3>Organizations</h3>
-                    <p>Most of the organizations can actually fit on one couch. It is not a problem. The problem is when the only place they spend time is the coach.</p>
+                    <p>Most of the nongovernmental organizations can fit on one couch. It is not a problem. The problem is when the only place they spend time is the coach.</p>
                 </li>
                 <li>
                     <img src='https://static.tildacdn.com/289c6bfa-02af-4579-955b-6a8f4a242f1b/effiencyl.png'>
@@ -74,136 +74,57 @@
             </div>
             <div class='col-sm-6'>
                 <div class="how-it-works">
-                    <h2>Як працює 3radar.org?</h2>
-                    <p>1. Ви кажете, якого обранця Ви хочете відкликати, а 3radar.org допомагає Вам знайти однодумців.</p>
-                    <p>2. Коли знайшлось достатньо однодумців, 3radar.org надає тренінгову підтримку відкликання обранців.</p>
-                    <p>3radar.org також планує допомагати (а) дізнаватись про діяльність обранців та (б) збирати ресурси, необхідні для тиску на них та їх відкликання.</p>
-                </div>
-            </div>
-        </div>
-        <div class='row with-pd'>
-            <div class='col-sm-8 col-sm-offset-2'>
-                <div class="faq">
-                    <h3>FAQ</h3>
-                    <strong>— А це законно?</strong>
-                    <p>— Так.</p>
-                    <strong>— Якими законами це регулюється?</strong>
-                    <p>
-                        — Законами України
-                        <a href='http://zakon2.rada.gov.ua/laws/show/595-19'>«Про місцеві вибори»</a>
-                        ,
-                        <a href='http://zakon2.rada.gov.ua/laws/show/93-15'>«Про статус депутатів місцевих рад»</a>
-                        та
-                        <a href='http://zakon2.rada.gov.ua/laws/show/280/97-%D0%B2%D1%80'>«Про місцеве самоврядування в Україні»</a>
-                        .
-                    </p>
-                    <strong>— Так же можна всіх обранців відкликати?</strong>
-                    <p>— Так. Крім депутатів Верховної ради.</p>
-                    <strong>— І доведеться обирати нових?</strong>
-                    <p>— Не завжди. У міських, районних та обласних радах, якщо відкликати депутата, на його місце прийде наступний по рейтингу у списку відповідної партії. Якщо список закінчиться, ці місця залишаться у раді вакантними до чергових виборів.</p>
-                    <strong>— Чи не використає хтось цей інструмент проти хороших обранців?</strong>
-                    <p>— Це може статись навіть з чудовими обранцем та обранкою. Як і завершення строку повноважень.</p>
-                    <strong>— І у владу потраплять погані обранці?</strong>
-                    <p>— Так. Але їх можна буде відкликати також.</p>
-                    <strong>— А якщо не вийде?</strong>
-                    <p>— То можна спробувати ще.</p>
-                    <strong>— На яку партію чи політика ви працюєте?</strong>
-                    <p>— На жодну. Навпаки - ми примушуємо їх працювати на громадян.</p>
-                    <strong>— Хто за вами стоїть?</strong>
-                    <p>— Ніхто, крім Вас й таких, як Ви. За нами наш досвід протидії фальсифікаціям виборів.</p>
-                    <strong>— Хто фінансує ваш проект?</strong>
-                    <p>— Такі громадяни, як Ви. Інколи — донори, яких ми не приховуємо.</p>
-                    <strong>— Хіба це не безкоштовно?</strong>
-                    <p>— Консультації та література безкоштовно. Тренінги потребують більшого часу, тому необхідна компенсація.</p>
-                    <strong>— Так це бізнес?!</strong>
-                    <p>— Ні. Але інколи допомога громадянам потребує фінансування. Ми не приховуємо джерел свого фінансування.</p>
-                    <strong>— Ви ж ніби проти заробляння на владі?!</strong>
-                    <p>— Так. Ми не чиновники й не заробляємо на владі та використанні державних повноважень. Ви не платите нам податки. Коли Ви купуєте пресу, Ви також фінансуєте прозорість, доступ до інформації та контроль за владою (звісно, якщо Ви купуєте справді незалежну та якісну пресу). Ми поширюємо інформацію про Ваші права та методи їх захисту переважно безкоштовно.</p>
-                    <strong>— Ви порвали мій шаблон!</strong>
-                    <p>— Не дуже боляче? Чудово! Ми любимо допомагати людям й робити це без насилля. :)</p>
-                    <strong>— Ви все це серйозно?</strong>
-                    <p>— Абсолютно!</p>
+                    <h2>How does CivicOS.net work?</h2>
+                    <p>1. CivicOS.net is a schema.org extension for civil society, which means it is a part of the world movement for efficiency, open data, freedom, democracy and human rights.</p>
+                    <p>2. It is a standard, which means it works best when/if accepted.</p>
+                    <p>3. It is voluntary, which means it is 100% up to you whether to use it.</p>
+                    <p>4. It is a project in the making, which means it is far from the ideal yet.</p>
+                    <p>5. It is a human idea, which means every human being can participate.</p>                               
+                    <p>6. It is based on the experiences of successes and failures, which means your experiences can be of great importance too.</p>
                 </div>
             </div>
         </div>
         <div class='row with-pd'>
             <div class="col-sm-12">
-                <h2 class='text-center'>Підтримка</h2>
+                <h2 class='text-center'>Support</h2>
                 <ul class='statistic-item'>
                     <li>
                         <img src='https://static.tildacdn.com/06a47c9d-5a6b-4e5e-bd93-d4a49e824646/111.png'>
-                        <h3>Консультації</h3>
+                        <h3>Consulting</h3>
                     </li>
                     <li>
                         <img src='https://static.tildacdn.com/070f7d1e-f126-4902-a382-c535db7b46ba/222.png'>
-                        <h3>Література</h3>
+                        <h3>Literature</h3>
                     </li>
                     <li>
                         <img src='https://static.tildacdn.com/7d8111e0-3b82-4e01-821d-b2da5feb1494/333.png'>
-                        <h3>Тренінги</h3>
+                        <h3>Trainings</h3>
                     </li>
                 </ul>
             </div>
         </div>
         <div class='row text-center with-pd'>
-            <a href="/me" class='black-btn'>Ну давайте вже когось відкликати</a>
+            <a href="/me" class='black-btn'>Join</a>
         </div>
-        <div class='row with-pd'>
-            <div class='col-sm-8 col-sm-offset-2'>
-                <div class="main-lit">
-                    <p>
-                        Відкликання депутатів місцевих рад, голів міст та сіл, передбачене законодавством України - законами
-                        <a href='http://zakon2.rada.gov.ua/laws/show/595-19'>«Про місцеві вибори»</a>
-                        ,
-                        <a href='http://zakon2.rada.gov.ua/laws/show/93-15'>«Про статус депутатів місцевих рад»</a>
-                        та
-                        <a href='http://zakon2.rada.gov.ua/laws/show/280/97-%D0%B2%D1%80'>«Про місцеве самоврядування в Україні»</a>
-                        .
-                    </p>
-                    <p>
-                        Втім, обранці можуть намагатись перешкоджати захисту прав та інтересів громадян й вдаватись до тиску. Як краще протидіяти репресіям й використати їх проти авторитарних обранців, можна дізнатись з багатьох джерел. Зокрема - з книги Протидія репресіям. Тут ми наводимо посилання на версію книги
-                        <a href='http://canvasopedia.org/wp-content/uploads/2015/08/MOB_English_May2014.pdf'>англійською</a>
-                        ,
-                        <a href='http://canvasopedia.org/wp-content/uploads/2016/10/MOBrusPreFinal.compressed.pdf'>російською</a>
-                        та
-                        <a href='http://www.everydayrebellion.net/methods/make-oppression-backfire/'>англомовний сайт про ненасильницький спротив</a>
-                        . Українське видання готується.
-                    </p>
-                    <p>
-                        Більше матеріалів про ненасильницькі методи боротьби проти диктаторів Ви знайдете на сайті
-                        <a href='http://www.nonviolent.solutions/'>Аґенції ненасильницьких рішень</a>
-                        .
-                    </p>
-                </div>
-            </div>
-            <div class='col-sm-2'>
-                <div class="book">
-                    <img src='https://static.tildacdn.com/tild6135-6138-4235-a334-313838333735/ProtivodeystvieRepressiyam.jpg' style='width: 100%;'>
-                    <p class='name'>Противодействие репрессиям</p>
-                    <p class='author'>Срджа Попович, Тори Порелл</p>
-                </div>
-            </div>
-        </div>
-    </div>
 <section class="section contact-section pb40">
     <div class="section-overlay"></div>
     <div class="section-center">
-        <h2 class="section-title">Зв'язок</h2>
-        <p>ми любимо допомагати захищати права</p>
+        <h2 class="section-title">Contact</h2>
+        <p>we love to help</p>
         <ul class="section-social">
             <li>
-                <a href="https://www.facebook.com/3radarorg-697777160381238/" target="_blank">
+                <a href="https://www.facebook.com/CivicOS.net" target="_blank">
                     <i class="fa fa-facebook"></i>
                 </a>
             </li>
             <li>
-                <a href="https://twitter.com/3radardotorg" target="_blank">
+                <a href="https://twitter.com/CivicOSnet" target="_blank">
                     <i class="fa fa-twitter"></i>
                 </a>
             </li>
         </ul>
-        <div class="contact-mail">Якщо Вам потрібна допомога, або у Вас виникла ідея партнерства, або Ви помітили помилку на нашому сайті, напишіть нам одразу: <a href="mailto:3radar.org@gmail.com">3radar.org@gmail.com</a></div>
-        <p><a href='http://www.nonviolent.solutions/'>Аґенція ненасильницьких рішень</a>, 2016</p>
+        <div class="contact-mail">Feel free to <a href="mailto:dimopot@gmail.com">contact</a></div>
+        <p><a href='http://www.nonviolent.solutions/'>Nonviolent Solutions Agency</a>, 2016</p>
     </div>
 </section>
 <script src="/js/jquery.min.js"></script>
