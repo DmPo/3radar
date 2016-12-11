e31067d392d05c6d116600253eafffe33cce0570
DmPo <dimopot@gmail.com>
2016-12-11 18:32:28 +0200

    Update layout.php

--- assets/views/layout.php
+++ assets/views/layout.php
@@ -14,15 +14,15 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="/new_campaign">Створити кампанію <span class="sr-only">(current)</span></a></li>
-                <li><a href="/campaigns">Кампанії</a></li>
+                <li class="active"><a href="/new_campaign">Start Campaign <span class="sr-only">(current)</span></a></li>
+                <li><a href="/campaigns">Campaigns</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="/me"><?= $user->first_name ?></a></li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <li><a href="/logout">Вийти</a></li>
+                        <li><a href="/logout">Logout</a></li>
                     </ul>
                 </li>
             </ul>
