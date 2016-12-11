960620857c248f127ba0185eec85dc5f586a7c98
DmPo <dimopot@gmail.com>
2016-12-11 16:36:24 +0200

    Update auth.php

--- assets/views/auth.php
+++ assets/views/auth.php
@@ -4,18 +4,18 @@
         <div class="form">
             <form class="register-form" method="post" action="/reg" name="signUpForm">
                 <input type="email" placeholder="E-Mail" name="email" ng-model="user.email" required/>
-                <input type="text" placeholder="Ім'я" name="first_name" ng-model="user.first_name" required/>
-                <input type="text" placeholder="Прізвище" name="last_name" ng-model="user.last_name" required/>
-                <input type="password" placeholder="Пароль" name="password" ng-model="user.password" required/>
-                <sup> мінімальна довжина паролю 6 символів</sup>
-                <input type="tel" placeholder="Контактний номер" name="phone" ng-model="user.phone" required/>
+                <input type="text" placeholder="Name" name="first_name" ng-model="user.first_name" required/>
+                <input type="text" placeholder="Surname" name="last_name" ng-model="user.last_name" required/>
+                <input type="password" placeholder="Password" name="password" ng-model="user.password" required/>
+                <sup> password must contain at least 6 characters</sup>
+                <input type="tel" placeholder="Contact phone" name="phone" ng-model="user.phone" required/>
 
                 <div class="g-recaptcha" data-callback="enableBtn"
                      data-sitekey="6Le_TAwUAAAAADlS9cv7s1ECFlo3QS2-fDRLyDL5"></div>
                 <p style="color: red; font-size: medium">{{signup_error}}</p>
 
-                <button id="signupbutton" type="button" ng-click="signup()">Зареєструватися</button>
-                <p class="message">Вже зареєстровані? <a href="#">Увійти</a></p>
+                <button id="signupbutton" type="button" ng-click="signup()">Sign Up</button>
+                <p class="message">Registered? <a href="#">Enter</a></p>
 
             </form>
             <form class="login-form" method="post" action="/auth">
@@ -26,18 +26,18 @@
                     <p style="color: red; font-size: medium"> <?= $error ?> </p>
                 <?php endif; ?>
                 <button>Увійти</button>
-                <p class="message">Не зареєстровані? <a href="#">Створити акаунт</a></p>
+                <p class="message">Not registered? <a href="#">New account</a></p>
             </form>
 
             <div class="social">
                     <a href="<?= $fb_login ?>" class="btn btn-primary" >
-                        <i class="fa fa-facebook" style="color: white"></i> Увійти за допомогою facebook
+                        <i class="fa fa-facebook" style="color: white"></i> Log in with your Facebook account
                     </a>
                     <a href="<?= $gl_login ?>" class="btn btn-danger" >
-                        <i class="fa fa-google" style="color: white"></i> Увійти за допомогою google
+                        <i class="fa fa-google" style="color: white"></i>Log in with your Google account
                     </a>
             </div>
-            <p><span class="small text-muted">Забули пароль? </span><a href="/recovery">Відновити</a></p>
+            <p><span class="small text-muted">Lost password? </span><a href="/recovery">Recover your password</a></p>
         </div>
     </div>
 </div>
