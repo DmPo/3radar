f67ceaea02074f711e126471b828e7e8ed930a88
DmPo <dimopot@gmail.com>
2016-12-11 19:31:22 +0200

    Update recovery.php

--- assets/views/recovery.php
+++ assets/views/recovery.php
@@ -4,19 +4,19 @@
         <?php if (isset($message)): ?>
             <div class="form">
                 <h2 class="h2 text-green"><?= $message ?></h2>
-                <p class="message"><a href="/auth">Увійти</a></p>
+                <p class="message"><a href="/auth">Enter</a></p>
             </div>
         <?php else : ?>
             <div class="form">
-                <h2 class="h2">Відновлення паролю</h2>
+                <h2 class="h2">Password recovery</h2>
                 <form class="login-form" method="post" action="/recovery">
                     <input type="email" placeholder="email" name="email"/>
                     <?php if (isset($error)): ?>
                         <p style="color: red; font-size: medium"> <?= $error ?> </p>
                     <?php endif; ?>
-                    <p class="p">введіть email, щоб ми вам надіслали посилання з відновленням доступу до сторінки.</p>
-                    <button>скинути пароль</button>
-                    <p class="message"><a href="/auth">Увійти</a></p>
+                    <p class="p">Your email is needed to reset the password.</p>
+                    <button>reset password</button>
+                    <p class="message"><a href="/auth">Enter</a></p>
                 </form>
             </div>
         <?php endif; ?>
